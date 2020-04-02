f1d0461c217bbbfab1ff3923c43a13e3fbce86d8
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2020-04-02 20:50:19 +0900

    implement AppComponent.myProcess()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface MyResult {
+  message: string;
+  result: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng-sentry-rxjs';
+  title = "ng-sentry-rxjs";
+
+  public myProcess(willSuccess: boolean): void {
+    this.getObservable(willSuccess).subscribe(result => console.log(result));
+  }
+
+  private getObservable(willSuccess: boolean): Observable<MyResult> {
+    return new Observable<MyResult>(observer => {
+      if (willSuccess) {
+        observer.next({ message: "hello, observable!!", result: willSuccess });
+      } else {
+        observer.error({
+          message: "my failure...",
+          result: willSuccess
+        });
+      }
+    });
+  }
 }
