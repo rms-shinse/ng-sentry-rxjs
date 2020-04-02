bbd50fcc046adad2229270e2c71a25b39f200788
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2020-04-02 20:57:46 +0900

    implement SentryErrorHandler
    `https://docs.sentry.io/platforms/javascript/angular/`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SentryErrorHandler } from "./sentry-error-handler";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/sentry-error-handler.ts
@@ -0,0 +1,17 @@
+import { ErrorHandler, Injectable } from "@angular/core";
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: ""
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
